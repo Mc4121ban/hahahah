57d2417a6c62ce1c0a47025426ab24ebfb1eab05
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2023-10-03 19:39:15 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,10 @@
+getgenv().config = getgenv().config or {
+  hit_time = 0.1,
+
+  mode = 'Always',
+  deflect_type = 'Remote',
+  notifications = false,
+  keybind = Enum.KeyCode.V
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mc4121ban/hahahah/main/update.lua",true))()

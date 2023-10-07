a348328925256b4b708949f8af27aab9972d2132
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2023-10-07 23:25:42 +0700

    Create n.lua

---
+++ n.lua
@@ -0,0 +1,7 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+OrionLib:MakeNotification({
+Name = "Blade Ball | Auto Parry",
+Content = "doitenroi.9941",
+Image = "rbxassetid://4483345998",
+Time = 12
+})

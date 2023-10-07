1db8cd2e75805fae34df9ace6e8d93c061036ce1
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2023-10-07 22:59:00 +0700

    Create fastattack.lua

---
+++ fastattack.lua
@@ -0,0 +1,12 @@
+shared.config = {
+   adjustment = 4,
+   hit_range = 0.9,
+
+   mode = 'Always',
+   deflect_type = 'Remote',
+   notifications = true,
+   keybind = Enum.KeyCode.E
+}
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Mc4121ban/hahahah/main/testv1.lua'),true))()
+
